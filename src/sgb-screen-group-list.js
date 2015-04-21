17ffb1c480e744082556d7af8dc0ff204e3b6a15
Juan Garcia <juan.garcia@synergy-gb.com>
2015-04-21 15:30:01 -0500

    Acualizando controlador para que acepte la data con el nombre adecuado

--- src/sgb-screen-group-list.js
+++ src/sgb-screen-group-list.js
@@ -2,7 +2,7 @@
 
 angular.module('sgb-screen-group-list', ['megazord'])
   .controller('sgb-screen-group-list-controller', ['$stateParams', '$scope', function($stateParams, $scope){
-    $scope.items = $stateParams.data;
+    $scope.sections = $stateParams.data;
 
     $scope.itemSearchHandler = function(){};
 
