2a1d85776ee3e9bfb7e87f04e8ccb9c37d6ce23a
Saul Hernandez <sauldhernandez@gmail.com>
2015-04-26 13:53:30 -0430

    Screen title can now be configured

--- dist/sgb-screen-group-list.js
+++ dist/sgb-screen-group-list.js
@@ -2,14 +2,15 @@
 'use strict';
 
 angular.module('sgb-screen-group-list', ['megazord'])
-  .controller('sgb-screen-group-list-controller', ['$stateParams', '$scope', function($stateParams, $scope){
-    $scope.sections = $stateParams.data;
+  .controller('sgb-screen-group-list-controller', ['$stateParams', '_screenParams','$scope', function($stateParams, _screenParams, $scope){
+      $scope.title = _screenParams.title || 'group_list_title';
+      $scope.sections = $stateParams.data;
 
-    $scope.itemSearchHandler = function(){};
+      $scope.itemSearchHandler = function(){};
 
-    $scope.itemSearchCancelHandler = function(){};
+      $scope.itemSearchCancelHandler = function(){};
 
-    $scope.itemClickHandler = function(){};
+      $scope.itemClickHandler = function(){};
 
   }]);
 },{}]},{},[1]);
