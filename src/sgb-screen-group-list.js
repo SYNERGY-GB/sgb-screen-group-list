1375a2ba33ee06a28823ba936b23fb136f753293
Juan Garcia <juan.garcia@synergy-gb.com>
2015-05-13 15:18:01 -0500

    Developing and testing for TechDay App

--- src/sgb-screen-group-list.js
+++ src/sgb-screen-group-list.js
@@ -1,14 +1,42 @@
 'use strict';
 
 angular.module('sgb-screen-group-list', ['megazord'])
-  .controller('sgb-screen-group-list-controller', ['$stateParams', '_screenParams','$scope', function($stateParams, _screenParams, $scope){
-      $scope.title = _screenParams.title || 'group_list_title';
-      $scope.sections = $stateParams.data;
+    .controller('sgb-screen-group-list-controller', ['_router', '_screenParams','$stateParams', '$scope', 'lodash', function(_router, _screenParams, $stateParams, $scope, _){
+        $scope.title = _screenParams.title || 'group_list_title';
+        $scope.sections = $stateParams.data;
 
-      $scope.itemSearchHandler = function(){};
+        $scope.items = $scope.sections.items;
+        $scope.searchQuery = "";
+        $scope.filteredItems = $scope.items;
+        $scope.showSearch = typeof(_screenParams.showSearch) === 'undefined'? true : _screenParams.showSearch;
 
-      $scope.itemSearchCancelHandler = function(){};
+        $scope.itemSearchHandler = function(){};
 
-      $scope.itemClickHandler = function(){};
+        $scope.itemSearchCancelHandler = function(){};
 
-  }]);
+        $scope.filterItems = function(searchQuery){
+            var search = searchQuery.toLowerCase();
+            $scope.filteredItems = _.filter($scope.items, function(item){
+                return (item.title && item.title.toLowerCase().indexOf(search) != -1) ||
+                    (item.detail1 && item.detail1.toLowerCase().indexOf(search) != -1) ||
+                    (item.detail2 && item.detail2.toLowerCase().indexOf(search) != -1) ||
+                    (item.url && item.url.toLowerCase().indexOf(search) != -1);
+            });
+        };
+
+        $scope.cancelSearch = function(){
+            $scope.searchQuery = "";
+            $scope.filteredItems = $scope.items;
+        };
+
+        $scope.itemClickHandler = function(item){
+            //Nothing to do but fire the event
+            _router.fireEvent({
+                name: 'itemClick',
+                params: {
+                    item: item
+                }
+            })
+        };
+
+    }]);
