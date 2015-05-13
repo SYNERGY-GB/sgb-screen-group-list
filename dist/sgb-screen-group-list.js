eb8eb2b27c8a9e9db420b6bfaf71966f8d2bb3b4
Saul Hernandez <sauldhernandez@gmail.com>
2015-05-13 15:43:58 -0430

    Fixes for init

--- dist/sgb-screen-group-list.js
+++ dist/sgb-screen-group-list.js
@@ -4,7 +4,7 @@
 angular.module('sgb-screen-group-list', ['megazord'])
   .controller('sgb-screen-group-list-controller', ['$stateParams', '_screenParams', '_screen', '$scope', function($stateParams, _screenParams, _screen, $scope){
 
-        _screen.init();
+        _screen.init($scope, _screenParams);
       $scope.title = _screenParams.title || 'group_list_title';
       $scope.stylesheet = _screenParams.stylesheet
       $scope.sections = $stateParams.data;
