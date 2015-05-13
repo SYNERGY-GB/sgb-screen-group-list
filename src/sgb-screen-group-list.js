794ba9b53985e5c65a8cab38504257777507bf0e
Saul Hernandez <sauldhernandez@gmail.com>
2015-05-13 15:39:34 -0430

    Auto class test

--- src/sgb-screen-group-list.js
+++ src/sgb-screen-group-list.js
@@ -1,8 +1,11 @@
 'use strict';
 
 angular.module('sgb-screen-group-list', ['megazord'])
-  .controller('sgb-screen-group-list-controller', ['$stateParams', '_screenParams','$scope', function($stateParams, _screenParams, $scope){
+  .controller('sgb-screen-group-list-controller', ['$stateParams', '_screenParams', '_screen', '$scope', function($stateParams, _screenParams, _screen, $scope){
+
+        _screen.init();
       $scope.title = _screenParams.title || 'group_list_title';
+      $scope.stylesheet = _screenParams.stylesheet
       $scope.sections = $stateParams.data;
 
       $scope.itemSearchHandler = function(){};
