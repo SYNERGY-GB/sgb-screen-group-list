20825e1215b8829f4b0cf3120654e6d74799ad05
Saul Hernandez <sauldhernandez@gmail.com>
2015-05-13 16:11:35 -0430

    class test

--- src/sgb-screen-group-list.js
+++ src/sgb-screen-group-list.js
@@ -5,7 +5,7 @@ angular.module('sgb-screen-group-list', ['megazord'])
 
         _screen.init($scope, _screenParams);
       $scope.title = _screenParams.title || 'group_list_title';
-      $scope.stylesheet = _screenParams.stylesheet
+      $scope.stylesheet = _screenParams.stylesheet;
       $scope.sections = $stateParams.data;
 
       $scope.itemSearchHandler = function(){};
