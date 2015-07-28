c400ff897039e15a7239fde15ea778a33f88d2ce
luismiglietti <luismiglietti@gmail.com>
2015-07-28 09:34:23 -0430

    templates dinamicos

--- src/sgb-screen-group-list.js
+++ src/sgb-screen-group-list.js
@@ -59,10 +59,15 @@ angular.module('sgb-screen-group-list', ['megazord'])
     .directive('mzTemplate', function() {
         return {
             restrict: 'EA',
-            replace: true, 
-            templateUrl: function (tElement, tAttrs) {
-                 //console.log('directive_templates/group-list-'+(scope.priority?'news':'regular')+'.html');
-                 return 'directive_templates/group-list-news.html'; 
+            transclude: true,
+            scope: {
+                user: '=data'
+            },
+            template: '<ng-include src="getTemplateUrl()"/>',
+            controller: function($scope) {
+                $scope.getTemplateUrl = function() {
+                    return 'directive_templates/group-list-'+$scope.user.priority+'.html';
+                }
             }
         }; 
     });
